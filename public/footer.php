8357c0fff1f03817029a76dd6574d8a0a2e7fb0d
200429-febry <febriyadi845@gmail.com>
2025-06-21 02:31:43 +0700

    Initial commit of complete web project

--- public/footer.php
+++
@@ -1,3 +0,0 @@
-
-        <div id="live-comment-feed">
-    </div>
--- public/register.php
+++
@@ -1,8 +0,0 @@
-<?php
-// public/register.php
-// This file acts as a PHP wrapper to include the static HTML form
-// and allows for PHP processing (e.g., displaying messages from URL parameters).
-
-include 'register.html';
-
-?>
